0da911efb0167a122711e28dba3ee4571d539df3
mathias <germannewsmaker@gmail.com>
2021-11-17 19:12:26 +0100

    Created the App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './images/profile.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="profile-pic" alt="logo"/>
+    </div>
+  );
+}
+
+export default App;
