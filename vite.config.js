3347360bfa1090fdcc97ed8c6a4ea5c12a439b7b
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-02 21:42:51 +0100

    Added aliases to the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import react from '@vitejs/plugin-react';
+import path from "path";
 
 export default {
     plugins: [react()],
-    build: {outDir: 'build'}
+    build: {outDir: 'build'},
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 }
