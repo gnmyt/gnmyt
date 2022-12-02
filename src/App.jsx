b52e580687918e6a47d2450b485485da89cf4652
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-02 21:16:43 +0100

    Cleaned up the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import logo from './images/profile.png';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-        <img src={logo} className="profile-pic" alt="logo"/>
-    </div>
-  );
-}
+export const App = () => (
+    <></>
+);
 
 export default App;
