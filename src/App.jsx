7229641683cbf4c0817470d349827d89957e7db5
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-03 16:30:29 +0100

    Integrated the Home page into the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "@/common/styles/main.sass";
+import "@/common/styles/_fonts.sass";
+import Home from "@/pages/Home";
 
 export const App = () => (
-    <></>
+    <>
+        <Home />
+    </>
 );
 
 export default App;
