b553e3069da619ec7c323b17c10925fb07b8c201
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-03 16:29:58 +0100

    Created the Home page component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import "./styles.sass";
+import {Button} from "@/common/components/Button";
+import {faEnvelope, faRocket} from "@fortawesome/free-solid-svg-icons";
+import ReactTyped from "react-typed";
+import ImageArea from "@/pages/Home/components/ImageArea";
+
+const Home = () => {
+    return (
+        <div className="home-wrapper">
+            <div className="left-area">
+                <div className="text-area">
+                    <h1>Hi, ich bin <span>GNM</span>.</h1>
+                    <h1>Ich <span><ReactTyped strings={["programmiere", "erstelle Konzepte", "bin 16 Jahre alt"]} typeSpeed={120} backSpeed={60}
+                                              loop/></span></h1>
+                </div>
+                <div className="button-area">
+                    <Button icon={faRocket} text="Projekte"/>
+                    <Button icon={faEnvelope} text="Kontakt"/>
+                </div>
+            </div>
+
+            <ImageArea />
+
+        </div>
+    );
+}
+
+export default Home;
