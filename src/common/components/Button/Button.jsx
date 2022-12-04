e2bc3f0ea04cb2ea926c00b867a9e72484ebdaa6
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-04 02:32:04 +0100

    The Button.jsx component now supports "to" links

--- src/common/components/Button/Button.jsx
+++ src/common/components/Button/Button.jsx
@@ -1,9 +1,12 @@
 import "./styles.sass";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom";
 
 export const Button = (props) => (
     <button className="btn btn-primary">
-        {props.icon && <FontAwesomeIcon icon={props.icon} className="icon" />}
-        {props.text}
+        <Link to={props.to}>
+            {props.icon && <FontAwesomeIcon icon={props.icon} className="icon"/>}
+            {props.text}
+        </Link>
     </button>
 );
