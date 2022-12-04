7d0c6a436e2b601e4502c21ac2e55a49ad445477
Mathias Wagner <germannewsmaker@gmail.com>
2022-12-04 02:32:32 +0100

    Integrated routes into the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "@/common/styles/main.sass";
 import "@/common/styles/_fonts.sass";
 import Home from "@/pages/Home";
+import Contact from "@/pages/Contact";
+import Projects from "@/pages/Projects";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 export const App = () => (
     <>
-        <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
     </>
 );
 
