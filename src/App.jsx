2c1131c287b5540ae698267e100ab43b59776f25
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-08 21:22:33 +0200

    Added routes to the privacy & imprint pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Home from "@/pages/Home";
 import Contact from "@/pages/Contact";
 import Projects from "@/pages/Projects";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Privacy from "@/pages/Privacy";
+import Imprint from "@/pages/Imprint";
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/privacy" element={<Privacy />} />
+                <Route path="/imprint" element={<Imprint />} />
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
