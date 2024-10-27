63203f2ef9b2675a649a9f36d60eedfdd36b0cce
Mathias Wagner <germannewsmaker@gmail.com>
2024-10-27 16:46:28 +0100

    Update sass to use modern compiler

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import path from "path";
 export default {
     plugins: [react()],
     build: {outDir: 'build'},
+    css: {
+        preprocessorOptions: {
+            sass: {
+                api: 'modern-compiler',
+            },
+        },
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
