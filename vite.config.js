dca9a8cef48e9f8fd56fb4122cfa07c7222f4772
Mathias Wagner <germannewsmaker@gmail.com>
2024-10-27 17:34:46 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react';
 import path from "path";
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
-export default {
+export default defineConfig({
     plugins: [react()],
     css: {
         preprocessorOptions: {
@@ -15,4 +16,4 @@ export default {
             '@': path.resolve(__dirname, './src'),
         },
     },
-}
+});
