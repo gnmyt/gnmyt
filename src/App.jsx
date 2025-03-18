b94c69da34ff9372918e1fc325eaeb6acac6c125
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-18 20:57:04 +0100

    Create App.jsx & index.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
 
 import "@/common/styles/main.sass";
+import Navigation from "@/common/components/Navigation";
+import {Outlet} from "react-router";
 
 export default () => (
     <>
+        <Navigation/>
 
+        <Outlet/>
     </>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import {Home} from "@/pages/Home/Home.jsx";
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {path: "/", element: <Home/>,},
+            {path: "/projects", element: <div>Projects</div>},
+            {path: "/contact", element: <div>Contact</div>}
+        ]
+    }
+]);
+
+root.render(<RouterProvider router={router}/>);
