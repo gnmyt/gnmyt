739d88b1e670b14f782617ff0ac5e0998052504c
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-18 20:56:57 +0100

    Create Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import "./styles.sass";
+
+export const Home = () => {
+    return (
+        <div className="home-page">
+            <h1>A <span>Full-Stack</span> Software Developer from Germany</h1>
+
+            <div className="orbit-container">
+                <div className="orbit-ring orbit-ring-outer">
+
+                </div>
+                <div className="orbit-ring orbit-ring-inner">
+
+                </div>
+            </div>
+        </div>
+    )
+}
