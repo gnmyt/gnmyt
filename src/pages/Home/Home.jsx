7b2be60bb4c7d5f23968b52353f4a36fbc620c0f
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-18 21:36:25 +0100

    Update circles on Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,11 +6,9 @@ export const Home = () => {
             <h1>A <span>Full-Stack</span> Software Developer from Germany</h1>
 
             <div className="orbit-container">
-                <div className="orbit-ring orbit-ring-outer">
-
-                </div>
                 <div className="orbit-ring orbit-ring-inner">
-
+                    <div className="orbit-ring orbit-ring-outer">
+                    </div>
                 </div>
             </div>
         </div>
