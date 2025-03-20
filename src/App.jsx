3293b3d23f77f49941c8702ae54d70deb45a30ba
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-20 12:37:44 +0100

    Implement Cursor in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import "@/common/styles/main.sass";
 import Navigation from "@/common/components/Navigation";
 import Footer from "@/common/components/Footer";
 import Background from "@/common/components/Background";
+import Cursor from "@/common/components/Cursor";
 import {BackgroundProvider} from "@/common/components/Background/BackgroundContext";
 import {Outlet} from "react-router";
 
 export default () => (
     <BackgroundProvider>
+        <Cursor/>
         <Background/>
         <Navigation/>
         <Outlet/>
