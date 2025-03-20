97a2ab3228a8918e87f221513961847e1057a0fb
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-20 18:34:45 +0100

    Add mobile optimization to Planet component

--- src/pages/Home/components/Planet/Planet.jsx
+++ src/pages/Home/components/Planet/Planet.jsx
@@ -1,11 +1,16 @@
 import "./styles.sass";
+import {motion} from "framer-motion";
 
 export const Planet = ({x, y, onClick, logo, name}) => {
     return (
-        <div key={name} className="planet" onClick={() => onClick(name)}
-             style={{transform: `translate(${x}px, ${y}px)`}}
-             title={name}>
+        <motion.div
+            key={name}
+            className="planet"
+            onClick={() => onClick(name)}
+            style={{transform: `translate(${x}px, ${y}px)`}}
+            title={name}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}>
             <img src={logo} alt={`${name} Logo`}/>
-        </div>
+        </motion.div>
     )
 }
