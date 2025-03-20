62dd647072c3eaa4608e8d42af990d850ae84482
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-20 15:40:56 +0100

    Integrate Projects page into index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import {createBrowserRouter, RouterProvider} from "react-router";
 import {Home} from "@/pages/Home/Home.jsx";
 import Imprint from "@/pages/Imprint";
 import PrivacyPolicy from "@/pages/Privacy";
+import Projects from "@/pages/Projects";
 
 
 const root = createRoot(document.getElementById('root'));
@@ -15,7 +16,7 @@ const router = createBrowserRouter([
         element: <App/>,
         children: [
             {path: "/", element: <Home/>},
-            {path: "/projects", element: <div>Projects</div>},
+            {path: "/projects", element: <Projects />},
             {path: "/contact", element: <div>Contact</div>},
             {path: "/imprint", element: <Imprint />},
             {path: "/privacy", element: <PrivacyPolicy />}
