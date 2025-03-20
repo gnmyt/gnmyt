cfdaef7b60790af4452d47984ee7d38078ac6a5c
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-20 18:04:07 +0100

    Add mobile optimization to Navigation component

--- src/common/components/Navigation/Navigation.jsx
+++ src/common/components/Navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import "./styles.sass";
 
 import ProfileImage from "@/common/images/profile.png";
 import {Link} from "react-router";
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 
 const navigation = {
     "Home": "/",
@@ -11,17 +11,29 @@ const navigation = {
 }
 
 export const Navigation = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => setMobileMenuOpen(!mobileMenuOpen);
+
     return (
-        <nav>
-            <Link className="title-area">
+        <nav className={mobileMenuOpen ? "mobile-open" : ""}>
+            <Link className="title-area" to="/">
                 <img src={ProfileImage} alt="Profile" className="profile-image"/>
                 <h1>GNM<span>.dev</span></h1>
             </Link>
+            <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+                <span className="hamburger-line"></span>
+                <span className="hamburger-line"></span>
+                <span className="hamburger-line"></span>
+            </button>
             <div className="nav-area">
                 {Object.keys(navigation).map(key => (
                     <Fragment key={key}>
-                        <Link className="nav-item" to={navigation[key]}>{key}</Link>
-                        {key !== Object.keys(navigation)[Object.keys(navigation).length - 1] && <div className="nav-divider" />}
+                        <Link className="nav-item" to={navigation[key]} onClick={() => setMobileMenuOpen(false)}>
+                            {key}
+                        </Link>
+                        {key !== Object.keys(navigation)[Object.keys(navigation).length - 1] &&
+                            <div className="nav-divider"/>}
                     </Fragment>
                 ))}
             </div>
