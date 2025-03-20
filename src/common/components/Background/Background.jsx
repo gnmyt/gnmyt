24bf6a8d4cb8f314a1776f650925866bf2c8458f
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-20 13:05:41 +0100

    Add animations to Background.jsx

--- src/common/components/Background/Background.jsx
+++ src/common/components/Background/Background.jsx
@@ -1,14 +1,19 @@
 import "./styles.sass";
 import {useBackground} from "@/common/components/Background/BackgroundContext.jsx";
+import {motion} from "framer-motion";
 
 export const Background = () => {
     const {circles} = useBackground();
 
     return (
         <div className="background">
-            {circles.map((circle, index) => (
-                <div 
-                    key={index}
+            {circles.map((circle) => (
+                <motion.div
+                    initial={{scale: 0}}
+                    animate={{scale: 1}}
+                    exit={{scale: 0}}
+                    transition={{duration: 0.8}}
+                    key={JSON.stringify(circle)}
                     className="background-circle"
                     style={{
                         left: circle.left,
@@ -16,7 +21,8 @@ export const Background = () => {
                         bottom: circle.bottom,
                         right: circle.right,
                         width: circle.size || '30rem',
-                        height: circle.size || '30rem'
+                        height: circle.size || '30rem',
+                        opacity: circle.opacity
                     }}
                 />
             ))}
